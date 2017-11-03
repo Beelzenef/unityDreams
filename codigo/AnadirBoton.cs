869974f2e50ce8cc3f9080f5df0410fd145aa57f
Elena G <elena.guzbla@gmail.com>
2017-11-03 09:43:46 +0100

    Clone buttons onClick

---
+++ codigo/AnadirBoton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AnadirBoton : MonoBehaviour {
+
+    // Boton a colar
+    GameObject primerBoton;
+    // Contenedor de botones
+    GameObject padreDeBotones;
+
+    void Start () {
+        primerBoton = GameObject.Find("btnPrimero");
+        padreDeBotones = GameObject.Find("Contenido");
+    }
+
+    void Update () {
+		
+	}
+
+    public void AnadirButton()
+    {
+        // Añado un clon dle boton btnPrimero como hijo de Contenido
+        Instantiate(primerBoton, padreDeBotones.GetComponent<Transform>());
+    }
+
+
+}
