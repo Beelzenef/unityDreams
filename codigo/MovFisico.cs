1d036bd25bc9f470e053d71aaaae17b14314fbec
Elena G <elena.guzbla@gmail.com>
2017-11-14 14:19:47 +0100

    movimiento cinematico vs fisico

---
+++ codigo/MovFisico.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MovFisico : MonoBehaviour {
+
+    public float velocidad = 30F;
+    public Rigidbody cuerpoRigido;
+    Vector3 fuerzaEmpuje;
+
+
+    void Start () {
+        cuerpoRigido = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        fuerzaEmpuje = new Vector3(Input.GetAxis("Horizontal") * velocidad, 0, Input.GetAxis("Vertical") * velocidad);
+        cuerpoRigido.AddForce(fuerzaEmpuje);
+	}
+}
---
+++ codigo/Trasladar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trasladar : MonoBehaviour {
+
+    public Transform origen;
+    public Transform direccion;
+
+    float velocidad = 0.01F;
+
+	void Update () {
+        origen.Translate(direccion.transform.position * velocidad);
+	}
+}
