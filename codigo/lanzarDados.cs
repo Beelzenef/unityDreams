2c4a24ade4452b019e9507913a9473f606bfa60b
Elena G <elena.guzbla@gmail.com>
2017-11-21 14:20:46 +0100

    Update lanzarDados.cs

--- codigo/lanzarDados.cs
+++ codigo/lanzarDados.cs
@@ -7,13 +7,11 @@ public class LanzarDados : MonoBehaviour {
     private int valorAleatorio = 0;
 
     public Rigidbody cuerpoRigido;
-    private Vector3 fuerzaVertical;
     private System.Random dado;
 
     void Start ()
     {
         dado = new System.Random();
-        fuerzaVertical = new Vector3(0.5F, 1, 0);
     }
 
     void FixedUpdate ()
